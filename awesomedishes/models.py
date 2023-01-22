eaa560778e544bb6a287aad333736cc93dd8da7a
CW1224 <xixiwang1224@gmail.com>
2023-01-22 23:07:58 +0000

    Added in the basic folders, pages and structure

--- awesomedishes/models.py
+++ awesomedishes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Import Django authentication user system
+from django.contrib.auth.models import User
+
+class Booking(models.Model):
+    
+
+class Registration(models.Model):
+
