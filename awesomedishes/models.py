781f6cb876f2b9d4d43c40cc47cd2572a6ce7e6e
CW1224 <xixiwang1224@gmail.com>
2023-01-23 23:13:09 +0000

    problem with opening application, resolved

--- awesomedishes/models.py
+++ awesomedishes/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Import Django authentication user system
-from django.contrib.auth.models import User
-
-class Booking(models.Model):
-    
-
-class Registration(models.Model):
-
